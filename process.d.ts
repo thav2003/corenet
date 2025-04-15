b4d79b8cb9158194c7e9a9d4e06c992b6611182e
thav2003 <vuthase172485@fpt.edu.vn>
2025-04-15 18:01:16 +0700

    fix type env

--- process.d.ts
+++ process.d.ts
@@ -1,7 +1,6 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     NEXT_PUBLIC_NETWORK: WalletAdapterNetwork;
-    NEXT_PUBLIC_BACKEND_URL: string;
     NEXTAUTH_URL: string;
     NEXTAUTH_SECRET: string;
   }
