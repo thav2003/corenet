bf738805128b1d65e3ae1f6c97332262aa73618f
thav2003 <vuthase172485@fpt.edu.vn>
2025-04-16 08:22:23 +0700

    disable eslint and add webpack module

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+  eslint: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
